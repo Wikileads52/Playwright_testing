a3197335353c9a4f8d808cf257423091715a04f6
Wikileads52 <g.prouvost.f@gmail.com>
2026-04-03 18:42:03 +0200

    adding another QABrains test case for removing item from cart

---
+++ tests/Bellatrix_Solution_WebApp_testing.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect, Page, Locator } from "@playwright/test";
+
+test("Proton rocket image visible", async ({page})=>{
+    await page.goto("https://demos.bellatrix.solutions/blog/");
+    const isLoaded = await page.locator('[data-href="/wiki/File:Proton_Zvezda_crop.jpg"]')
+    .evaluate(img => { 
+        const image = img as HTMLImageElement
+        return image.complete && image.naturalWidth > 0});
+        expect(isLoaded).toBe(true);
+});
